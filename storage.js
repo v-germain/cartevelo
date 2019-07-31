82fce3585039e18857ad8fb72bddb70f944f9b1c
v-germain <vassili.germain@gmail.com>
2019-07-31 19:28:53 +0200

    canvas2

--- storage.js
+++ storage.js
@@ -2,10 +2,12 @@
 
 const bookingButton = document.getElementById("booking");
 
-bookingButton.addEventListener("click", ()=> {
+bookingButton.addEventListener("click", () => {
     const lastName = document.getElementById("lastname").value;
     const firstName = document.getElementById("firstname").value;
     localStorage.setItem("lastName", lastName);
     localStorage.setItem("firstName", firstName);
     console.log(localStorage.getItem("firstName"), localStorage.getItem("lastName"));
+    document.getElementById("canvas_container").classList.remove("none");
+    document.getElementById("canvas_container").classList.add("display");
 });

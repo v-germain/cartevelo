8d3ca31f1d02587025f7e042d3bb889e36abfcc7
v-germain <vassili.germain@gmail.com>
2019-07-31 19:00:13 +0200

    canvas

---
+++ canvas.js
@@ -0,0 +1,42 @@
+// création d'une classe canvas
+class Canvas {
+  constructor() {
+    this.canvas = document.getElementById("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.draw = false;
+    this.signature();
+  }
+    // récupération de la position de la souris par rapport au viewport
+    getMousePosition(e) {
+        let rectangle = this.canvas.getBoundingClientRect(e);
+        return {
+            x: e.offsetX - rectangle.left,
+            y: e.offsetY - rectangle.top,
+        }   
+    }
+
+    // création de la fonction pour tracer avec la position de la souris
+    signature() {
+        this.canvas.addEventListener("mousedown", (e) => {
+            let mousePosition = this.getMousePosition(e);
+            this.draw = true;
+            this.ctx.lineWidth = 2;
+            this.ctx.moveTo(mousePosition.x, mousePosition.y);
+            this.ctx.beginPath();
+        });
+
+        this.canvas.addEventListener("mousemove", (e) => {
+            // mousemove + draw = true => lancement de la la fonction
+            if (this.draw === true) {
+                this.ctx.lineTo(e.offsetX, e.offsetY);
+                // méthode stroke pour dessiner le chemin défini avec lineTo
+                this.ctx.stroke();
+            }
+        });
+
+        //fin du clique, draw = false, fin de la signature
+        this.canvas.addEventListener('mouseup', (e) => {
+          this.draw = false;
+        });
+    }
+}
--- init.js
+++ init.js
@@ -1,15 +1,4 @@
 const map = new Map();
 map.createMarkers();
 // initialisation des paramètres du canvas
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-// remplissage du dessin en vert
-ctx.fillStyle = 'green';
-/* positionnement du canvas (x, y, larg, haut)
-fillRect(x, y, largeur, hauteur) pour triangle plein
-strokeRect(x, y, largeur, hauteur) pour contour de rectangle
-clearRect(x, y, largeur, hauteur) pour rendre une zone rectangulaire transparante*/
-ctx.fillRect(10, 10, 100, 100);
-
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d', {alpha: false});
+const canvas = new Canvas();
